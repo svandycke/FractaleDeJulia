b1c3987abea323342f7b83057452b4075d7e7349
BENNANI Abdellatif <bennani_abdellatif@orange.fr>
2016-11-04 15:24:06 +0100

    window.cpp : iterations

--- window.cpp
+++ window.cpp
@@ -11,10 +11,14 @@ using namespace std;
 
 int main(int argc, char ** argv) {
 
-  Mat img(800, 600, CV_8UC3);
+  Mat img(600, 600, CV_8UC3);
   cvNamedWindow("Fractale de julia", CV_WINDOW_AUTOSIZE);
-  setWindowProperty("Fractale de julia", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+  moveWindow("Fractale de julia", 0, 0);
+  //setWindowProperty("Fractale de julia", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
   while(1) {
+    for(int i = 0; i < img.rows; i++)
+      for(int j = 0; j < img.cols; j++)
+        img.at<cv::Vec3b>(cv::Point(i, j)) = Vec3b(125, 125, 125);
     imshow("Fractale de julia", img);
     if((waitKey(10) & 0xFF) == 'q'){
       exit(-1);
