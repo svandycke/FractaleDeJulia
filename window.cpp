e2cf690f002f4fa0b6a7c04c836f59e6d47f7139
tifoo <tifoo@creature_du_bocal.(none)>
2016-11-05 14:58:45 +0100

    integration dans la boucle d'affichage

--- window.cpp
+++ window.cpp
@@ -11,6 +11,10 @@ using namespace cv;
 using namespace std;
 
 
+long double update(long double& number){
+  return (( (number += 0.00005) < 1. )? number : (number -= 2.));
+} 
+
 Vec3b julia(long double i, long double j, complex<long double> c){
   complex<long double> z(i, j);
   int val = 0;
@@ -18,18 +22,19 @@ Vec3b julia(long double i, long double j, complex<long double> c){
     z = z * z + c;
     val++;
   }while((val < 255) && (norm(z) < 2));
-  return Vec3b(0, val, 0);
+  return Vec3b(val/2, val/3, val);
 }
 
 int main(int argc, char ** argv) {
-  Mat img(600, 600, CV_8UC3);
+  Mat img(600, 600, CV_8UC3);//img(cols, rows, flags)
+  long double reel(-0.0986), imaginaire(-0.65186);
   cvNamedWindow("Fractale de julia", CV_WINDOW_AUTOSIZE);
   moveWindow("Fractale de julia", 0, 0);
-  complex<long double> c(-0.0986, -0.65186);
-  for(int i = 0; i < img.rows; i++)
-    for(int j = 0; j < img.cols; j++)
-      img.at<cv::Vec3b>(cv::Point(i, j)) = julia(((long double)i/img.rows) * 2. - 1., ((long double)j/img.cols) * 2. - 1. , c);
   while(1) {
+    complex<long double> c(update(reel), update(imaginaire));
+    for(int i = 0; i < img.rows; i++)
+      for(int j = 0; j < img.cols; j++)
+	img.at<Vec3b>(Point(j, i)) = julia(((long double)i/img.rows) * 2. - 1., ((long double)j/img.cols) * 2. - 1. , c);
     imshow("Fractale de julia", img);
     if((waitKey(10) & 0xFF) == 'q' || (waitKey(10) & 0xFF) == 27){
       exit(-1);
